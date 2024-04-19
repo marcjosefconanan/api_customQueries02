05d126f23442202f01e4b56da5b6693648f40972
Marc Josef Conanan <coanananmarc@gmail.com>
2024-04-19 18:12:46 +0200

    completato api customQueries02

---
+++ src/test/java/com/api_customQueries02/api_customQueries02/ApiCustomQueries02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_customQueries02.api_customQueries02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCustomQueries02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
